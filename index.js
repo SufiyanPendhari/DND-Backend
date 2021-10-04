16bde8d255d7666001c6d9f52be4d1cb929557fd
Sufiyan Pendhari <89012549+SufiyanPendhari@users.noreply.github.com>
2021-10-04 14:15:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = require('./routes/route')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://pendharisufiyan:uRH4v7rhjgf2Z6-@cluster0.u6mw7.mongodb.net/Data?retryWrites=true&w=majority').then(()=>{console.log('Connected to database')
+mongoose.connect("enter the database url here").then(()=>{console.log('Connected to database')
 app.listen(5000,()=> console.log('server started'))
 })
 
